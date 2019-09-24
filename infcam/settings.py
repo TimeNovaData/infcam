f924126a06cfcdbcb50320170de7ee5def93ef14
edsonjunior <edsondelimajunior@gmail.com>
2019-09-24 15:38:58 -0300

    TRE00012 - Configurar servidor local

--- infcam/settings.py
+++ infcam/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '192.168.1.137', 'infcam.dyndns.tv']
 
 
 # Application definition
